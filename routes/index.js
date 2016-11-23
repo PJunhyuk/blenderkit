2ccc6bda6e52450fe2d8448ff6619b5b4e6e4bd1
PJunhyuk <jgravity@naver.com>
2016-11-23 15:48:44 +0900

    Add blend page, and Make route to it

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blenderkit' });
+});
+
+/* make route to blend page */
+router.get('/blend', function(req, res, next) {
+  res.render('blend', { title: 'Blenderkit' });
 });
 
 module.exports = router;
