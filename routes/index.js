3484c2bd391503ffa19c9edc1b10c21ec31351b2
PJunhyuk <jgravity@naver.com>
2016-11-30 22:31:26 +0900

    Change blend page style, and Add blended-list page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/blend', function(req, res, next) {
   res.render('pages/blend', { title: 'Blenderkit' });
 });
 
+/* make route to blended-list page */
+router.get('/blended/list', function(req, res, next) {
+  res.render('pages/blended-list', { title: 'Blenderkit' });
+});
+
 module.exports = router;
